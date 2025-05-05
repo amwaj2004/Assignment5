1b46386c2565e14d29c32f8de7a76d4f2dcf6031
Kayln Shaw <kaylns@Kaylns-MacBook-Pro.local>
2025-05-04 22:27:04 -0500

    added the ExoticAnimal and ExoticPet

--- src/main/java/edu/mu/mvc/adapter/ExoticAnimal.java
+++ src/main/java/edu/mu/mvc/adapter/ExoticAnimal.java
@@ -1,5 +1,27 @@
 package edu.mu.mvc.adapter;
 
 public class ExoticAnimal {
+	String id;
+	String name;
+	String catagory;
+	String subSpecies;
+	int age;
+	
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getCatagory() {
+		return catagory;
+	}
+	public String getSubSpecies() {
+		return subSpecies;
+	}
+	public int getAge() {
+		return age;
+	}
 
+	
 }
