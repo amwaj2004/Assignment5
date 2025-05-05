d6ae035382d43e1a23ee24d5f7e4685928e08371
Kayln Shaw <kaylns@Kaylns-MacBook-Pro.local>
2025-05-04 23:46:28 -0500

    added petcontroller, ran out of time

--- src/main/java/edu/mu/mvc/controller/PetController.java
+++ src/main/java/edu/mu/mvc/controller/PetController.java
@@ -1,5 +1,26 @@
 package edu.mu.mvc.controller;
 
-public class PetController {
+import edu.mu.mvc.Pet;
+import edu.mu.mvc.model.Shelter;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileReader;
+
+import com.google.gson.*;
+
+public class PetController {
+	Shelter<Pet> shelter;
+
+	public PetController(Shelter<Pet> shelter) {
+		super();
+		this.shelter = shelter;
+	}
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	
+	public void loadFromJson(String file) {
+		try(FileReader scanner = new FileReader(file)){
+			
+		}
+	}
 }
